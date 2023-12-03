43a2b2ce656601df01b0483c4e0643334ed38c5e
Yonis123 <ynur472@gmail.com>
2023-12-03 04:29:37 -0500

    almost finished multi-step form styling

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -10,7 +10,6 @@ const circle_arr = [...circles.querySelectorAll("[circle]")]
 
 
 
-
 let currentStep = formSteps.findIndex(step => {
     return step.classList.contains("active")
 })
@@ -79,36 +78,45 @@ const no = document.getElementById('no');
 const other = document.getElementById('resolved_other')
 const resolved = document.getElementById('resolved_incident')
 
+function hover(){
+   this.style.backgroundColor = 'rgba(223, 223, 223, 0.92)'
+}
+function hover_out(){
+   this.style.backgroundColor = 'white'
+}
 
 // Add event listeners
 radio1.addEventListener('change', function() {
   if (radio1.checked) {
+    yes.removeEventListener('mouseover', hover)
+    yes.removeEventListener('mouseout', hover_out)
     other.classList.remove('do_not_show')
     resolved.style.marginBottom = '0px'
     yes.style.backgroundColor = 'rgba(223, 223, 223, 0.92)'
     yes.style.border = '2px solid black'
     no.style.backgroundColor = 'white'
     no.style.border = '1px solid black'
-    // no.addEventListener('mouseover', function() {
-    //   no.style.backgroundColor = 'rgba(223, 223, 223, 0.92)';
-    // });
-    // no.addEventListener('mouseout', function() {
-    //   no.style.backgroundColor = 'white';
-    // });
+    no.addEventListener('mouseover', hover)
+    no.addEventListener('mouseout', hover_out )
+
   }
 });
+
 
 // fix this issue 
 
 radio2.addEventListener('change', function() {
   if (radio2.checked) {
+    no.removeEventListener('mouseover', hover)
+    no.removeEventListener('mouseout', hover_out)
     other.classList.add('do_not_show')
     resolved.style.marginBottom = '24px'
     no.style.backgroundColor = 'rgba(223, 223, 223, 0.92)'
     no.style.border = '2px solid black'
     yes.style.backgroundColor = 'white'
     yes.style.border = '1px solid black'
-
+    yes.addEventListener('mouseover', hover)
+    yes.addEventListener('mouseout', hover_out )
   }
 });
 
@@ -118,37 +126,59 @@ radio2.addEventListener('change', function() {
 
 let people_involved = document.getElementById("people_involved");
 
+const people_involved_form = document.querySelector('[form-witness]')
+
+  
+const potential_people_involved =[...people_involved_form.querySelectorAll('[witness-step]')]
+
+potential_people_involved.forEach(element => {
+        // element.classList.add('do_not_show !important')
+        element.remove()
+    })
 
 people_involved.addEventListener("change", function() {
   // Get the value of the selected option
   let selectedOption = this.value
   console.log(selectedOption)
 
-
-  const people_involved_form = document.querySelector('[form-witness]')
-  
-  const potential_people_involved =[...people_involved_form.querySelectorAll('[witness-step]')]
+  const selector_wit = document.getElementById('people_involved')
     
   if (selectedOption == 0){
-    potential_people_involved.forEach(element => {
-        element.classList.add('do_not_show')
-    })
+      potential_people_involved.forEach(element => {
+          // element.classList.add('do_not_show !important')
+          element.remove()
+      })
+
+      selector_wit.style.marginBottom = '40px'
+      
   }else if (selectedOption == 1){
-    potential_people_involved.forEach(element => {
-        element.classList.add('do_not_show')
-    })
-    potential_people_involved[0].classList.remove('do_not_show')
+      potential_people_involved.forEach(element => {
+          element.remove()
+      })
+      
+      people_involved_form.appendChild(potential_people_involved[0])
+
+      selector_wit.style.marginBottom = '10px'
 
   }else if (selectedOption == 2){
-    potential_people_involved.forEach(element => {
-        element.classList.add('do_not_show')
-    })
-    potential_people_involved[0].classList.remove('do_not_show')
-    potential_people_involved[1].classList.remove('do_not_show')
+      potential_people_involved.forEach(element => {
+          element.remove()
+      })
+      people_involved_form.appendChild(potential_people_involved[0])
+      people_involved_form.appendChild(potential_people_involved[1])
+
+      selector_wit.style.marginBottom = '10px'
+
   } else if(selectedOption == 3){
-    potential_people_involved[0].classList.remove('do_not_show')
-    potential_people_involved[1].classList.remove('do_not_show')
-    potential_people_involved[2].classList.remove('do_not_show')
+      potential_people_involved.forEach(element => {
+          element.remove()
+      })
+      people_involved_form.appendChild(potential_people_involved[0])
+      people_involved_form.appendChild(potential_people_involved[1])
+      people_involved_form.appendChild(potential_people_involved[2])
+
+      selector_wit.style.marginBottom = '10px'
+      
   } else{
     //we need to make a pop up that tells them to add the people names and any relevant information to the comments portion or something
   }
