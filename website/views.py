535f9e2323589a037778d7a5e6c3879cefe43050
Yonis Nur <ynur472@gmail.com>
2023-12-07 03:45:35 -0500

    some stuff

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 views = Blueprint('views', __name__)
@@ -6,10 +6,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     
+    # return render_template('questions.html')
+    return 'this is the home page'
+        
+        
+@views.route('/make_report', methods=['GET', 'POST'])
+def make_report():
+    if request.method == 'POST':
+        incident_report_data = request.form
+        # now I want to use this data to generate an incident report, and save that pdf. Once I have it I want
+        # to save that pdf, send and send off the email with the pdf and then also i need to do db stuff for that pdf 
+        
+        
+        
+        
+        
     return render_template('questions.html')
-        
-        
-        
+   
         
         
         
